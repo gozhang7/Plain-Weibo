d9ad102746ff06ff1607317128ee4b5db1fc8339
tibeT7 <zhangsan8006@gmail.com>
2014-07-18 14:39:34 -0400

    Basic structure of Plain Weibo website

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function login() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    authorize(username, password);
+}
+
+function authorize(username, password) {
+	
+}
